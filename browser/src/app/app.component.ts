44950454c7031357699d0f6a108248b3be3eec14
igrigorev <igrigorev@abanking.ru>
2024-03-12 20:17:10 +0500

    rxJs

--- browser/src/app/app.component.ts
+++ browser/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NAME } from '../submodule';
 import { TextControlComponent } from './modules/components/text-control.component';
+import { AccComponent } from './components/account/account.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TextControlComponent],
+  imports: [RouterOutlet, TextControlComponent, AccComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- browser/src/app/components/account/account.component.ts
+++ browser/src/app/components/account/account.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { TextControlComponent } from '../../modules/components/text-control.component';
+import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
+interface ITask {
+    name: string;
+    color: string;
+}
 @Component({
     standalone: true,
     selector: 'app-acc',
-    template: 'Account',
+    template: `
+        <div>
+            <button (click)="addTask()">Добавить таску</button>
+            <div class="task-container" *ngIf="(tasks$ | async) as tasks">
+                <div class="task" *ngFor="let task of tasks" [style.margin-bottom]="'16px'" [style.background-color]="task.color">
+                    {{task.name}}
+                </div>
+            </div>
+        </div>
+    `,
     imports: [
-        TextControlComponent
-    ]
+        TextControlComponent,
+        CommonModule,
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
+export class AccComponent implements OnInit, OnDestroy {
+    public get tasks$(): Observable<ITask[]> {
+        return this._tasks$.asObservable();
+    }
 
-export class AccComponent implements OnInit {
+    protected _tasks$: BehaviorSubject<ITask[]> = new BehaviorSubject<ITask[]>([]);
+    private destroy$: Subject<void> = new Subject<void>();
     constructor() {
+        this.tasks$
+            .pipe(
+                takeUntil(this.destroy$)
+            )
+            .subscribe();
     }
 
     ngOnInit() {
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+    }
+
+    protected addTask(): void {
+        const curTask: ITask[] = this._tasks$.value;
+        curTask.push({
+            name: 'Я таска!',
+            color: 'red'
+        })
+
+        this._tasks$.next(curTask);
     }
 }
