879f3f3b49ddb6890b6661666988b97addfba2e3
Arina Topor <115094753+ArinaTopor@users.noreply.github.com>
2024-03-27 21:53:47 +0500

    init taiga-ui (#3)

--- apps/atm-browser/src/app/app.component.ts
+++ apps/atm-browser/src/app/app.component.ts
@@ -1,15 +1,30 @@
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import {
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TUI_SANITIZER,
+    TuiTextfieldControllerModule
+} from '@taiga-ui/core';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TuiInputModule } from '@taiga-ui/kit';
 
 @Component({
     standalone: true,
-    imports: [NxWelcomeComponent, RouterModule],
+    imports: [NxWelcomeComponent, RouterModule, TuiRootModule, TuiDialogModule, TuiAlertModule, CommonModule, TuiInputModule, ReactiveFormsModule, TuiTextfieldControllerModule],
     selector: 'atm-project-root',
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }]
 })
 export class AppComponent {
     protected title: string = 'atm-browser';
+    private readonly _testForm: FormGroup = new FormGroup({
+        testValue: new FormControl('profit@cool.ru'),
+    });
 }
