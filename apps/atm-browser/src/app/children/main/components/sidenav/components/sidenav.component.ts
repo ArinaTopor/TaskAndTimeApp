e47388d13c59e75deb415be70f88fe78cdd692a5
Arina Topor <arinatopor695@gmail.com>
2024-05-09 23:09:32 +0500

    fix styles

--- apps/atm-browser/src/app/children/main/components/sidenav/components/sidenav.component.ts
+++ apps/atm-browser/src/app/children/main/components/sidenav/components/sidenav.component.ts
@@ -14,6 +14,7 @@ import { ModeToggleStorageService } from '../../mode/services/mode-storage.servi
 import { FirebaseAuthService, USER_INFO_TOKEN } from '@atm-project/common';
 import firebase from 'firebase/compat/app';
 import { Observable, filter, of } from 'rxjs';
+import { NewProjectComponent } from '../../../children/project/children/modal-component/new-project.component';
 @Component({
     selector: 'sidenav',
     templateUrl: './sidenav.component.html',
@@ -24,6 +25,7 @@ import { Observable, filter, of } from 'rxjs';
         SkeletonLoadingComponent,
         ModeToggleService,
         ModeToggleStorageService,
+        NewProjectComponent,
     ],
 })
 export class SidenavComponent implements OnInit {
